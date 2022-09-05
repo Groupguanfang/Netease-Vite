c173d1362d4b1dae8e183027f6378159cdf8334d
古欢 <1203970284@qq.com>
2022-09-05 20:52:04 +0800

    2022/9/4

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="settings home padding">
     <Title title="设置"></Title>
+    <div class="settings-container">
+      <div class="item">
+        <div class="label">是否开启暗黑模式</div>
+        <input type="checkbox" @change="changeMode()" v-model="darkMode" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Title from "../components/Title.vue"
 export default {
+  data() {
+    return {
+      darkMode: false
+    }
+  },
+  methods: {
+    changeMode() {
+      console.log(this.darkMode)
+    }
+  },
   components: { Title }
 }
 </script>
